1c20a96ba08d8c2e34491ce99ea9f2c69a381da2
Santiago Pozo Ruiz <38196801+bytesantiago@users.noreply.github.com>
2024-12-04 20:01:40 -0500

    Update ContactPage.js

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -13,18 +13,18 @@ function ContactPage() {
       </div>
       <div className="ContactPage">
         <div className="contact-sect">
-          <ContactItem icon={phone} text1={"+593 994449269"} title={"Phone"} />
+          // <ContactItem icon={phone} text1={"+593 994449269"} title={"Phone"} />
           <ContactItem
             icon={email}
             text1={"santiagopozoruiz@gmail.com"}
             title={"Email"}
           />
-          <ContactItem
-            icon={location}
-            text1={"Quito, 170514"}
-            text2={"Ecuador"}
-            title={"Address"}
-          />
+          // <ContactItem
+          //   icon={location}
+          //   text1={"Quito, 170514"}
+          //   text2={"Ecuador"}
+          //   title={"Address"}
+          // />
         </div>
       </div>
     </div>
